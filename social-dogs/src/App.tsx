235a4aa39a971fb1dad5c8ddd810069de2f904ee
GuiDev45 <bortolettodev@gmail.com>
2023-11-25 18:22:48 -0300

    Organizando arquivos

--- social-dogs/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <p className="text-4xl font-bold underline text-blue-500">Teste</p>
-    </>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Teste from "./pages/TailWindTeste/teste";
+
+export default function App() {
+  return (
+    <>
+      <Teste />
+    </>
+  );
+}
